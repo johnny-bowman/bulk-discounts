3e00fbbe4472f3ef749c4b98fa5c330629db5e99
Johnny Bowman <johntbowman.production@gmail.com>
2022-04-23 14:26:48 -0600

    all tests pass

--- app/poros/repository.rb
+++
@@ -1,7 +0,0 @@
-class Repository
-  attr_reader :name
-
-  def initialize(data)
-    @name = data[:name]
-  end
-end
--- app/services/github_service.rb
+++
@@ -1,6 +0,0 @@
-class GithubService < BaseService
-  def self.get_repo_data
-    response = conn('https://api.github.com').get('/repos/phillipkamps/little-esty-shop')
-    get_json(response)
-  end
-end
