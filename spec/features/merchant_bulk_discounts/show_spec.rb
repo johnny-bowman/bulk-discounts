6c7672cbdd8aefa295540ed678efa3caae31d7f6
Johnny Bowman <johntbowman.production@gmail.com>
2022-04-23 19:22:42 -0600

    Test: bulk_discount edit page can update a discount

--- spec/features/merchant_bulk_discounts/show_spec.rb
+++ spec/features/merchant_bulk_discounts/show_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe "BulkDiscounts index page" do
+RSpec.describe "BulkDiscounts show page" do
   before :each do
     @merch_1 = Merchant.create!(name: "Two-Legs Fashion")
     @merch_2 = Merchant.create!(name: "Two-Legs Fashion")
@@ -11,10 +11,16 @@ RSpec.describe "BulkDiscounts index page" do
 
     @bd_4 = @merch_2.bulk_discounts.create!(percent_discount: 50, quantity_threshold: 25)
 
-    visit "/merchants/#{@merch_1.id}/bulk_discounts/#{@bd_1.id}"
+    visit "/merchants/#{@merch_1.id}/bulk_discounts/#{@bd_1.id}?merchant_id=#{@merch_1.id}"
   end
 
   it "displays discount quantity_threshold and percent_discount" do
     expect(page).to have_content("Percent Discount: 20\nQuantity Threshold: 10")
+  end
+
+  it "has link to edit discount" do
+    click_link("Edit Discount")
+
+    expect(current_path).to eq("/merchants/#{@merch_1.id}/bulk_discounts/#{@bd_1.id}/edit")
   end
 end
