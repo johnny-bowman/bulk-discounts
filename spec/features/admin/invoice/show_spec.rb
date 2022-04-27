664c9d34c624635a1eb9c954a0e26b1b763814ae
Johnny Bowman <johntbowman.production@gmail.com>
2022-04-26 22:57:43 -0600

    Feat: Admin invoice show page displays total revenue after discounts

--- spec/features/admin/invoice/show_spec.rb
+++ spec/features/admin/invoice/show_spec.rb
@@ -4,51 +4,33 @@ RSpec.describe "Invoice Show Page" do
   before :each do
     @merchant_1 = Merchant.create!(
       name: "Store Store",
-      created_at: Date.current,
-      updated_at: Date.current
     )
     @merchant_2 = Merchant.create!(
       name: "Erots",
-      created_at: Date.current,
-      updated_at: Date.current
     )
-
     @cup = @merchant_1.items.create!(
       name: "Cup",
       description: "What the **** is this thing?",
       unit_price: 10000,
-      created_at: Date.current,
-      updated_at: Date.current
     )
     @soccer = @merchant_1.items.create!(
       name: "Soccer Ball",
       description: "A ball of pure soccer.",
       unit_price: 32000,
-      created_at: Date.current,
-      updated_at: Date.current
     )
-
     @beer = @merchant_2.items.create!(
       name: "Beer",
       description: "Happiness <3",
       unit_price: 100,
-      created_at: Date.current,
-      updated_at: Date.current
     )
-
     @customer_1 = Customer.create!(
       first_name: "Malcolm",
       last_name: "Jordan",
-      created_at: Date.current,
-      updated_at: Date.current
     )
     @customer_2 = Customer.create!(
       first_name: "Jimmy",
       last_name: "Felony",
-      created_at: Date.current,
-      updated_at: Date.current
     )
-
     @invoice_1 = @customer_1.invoices.create!(
       status: 1,
       created_at: Date.new(2020, 12, 12),
@@ -56,23 +38,16 @@ RSpec.describe "Invoice Show Page" do
     )
     @invoice_2 = @customer_1.invoices.create!(
       status: 2,
-      created_at: Date.current,
-      updated_at: Date.current
     )
     @invoice_3 = @customer_2.invoices.create!(
       status: 0,
-      created_at: Date.current,
-      updated_at: Date.current
     )
-
     @invoice_item_1 = InvoiceItem.create!(
       item_id: @soccer.id,
       invoice_id: @invoice_1.id,
       quantity: 1,
       unit_price: @soccer.unit_price,
       status: 1,
-      created_at: Date.current,
-      updated_at: Date.current
     )
     @invoice_item_2 = InvoiceItem.create!(
       item_id: @cup.id,
@@ -80,8 +55,6 @@ RSpec.describe "Invoice Show Page" do
       quantity: 50,
       unit_price: @cup.unit_price,
       status: 1,
-      created_at: Date.current,
-      updated_at: Date.current
     )
     @invoice_item_3 = InvoiceItem.create!(
       item_id: @soccer.id,
@@ -89,20 +62,14 @@ RSpec.describe "Invoice Show Page" do
       quantity: 500,
       unit_price: @soccer.unit_price,
       status: 0,
-      created_at: Date.current,
-      updated_at: Date.current
     )
-
     @invoice_item_4 = InvoiceItem.create!(
       item_id: @beer.id,
       invoice_id: @invoice_3.id,
       quantity: 2,
       unit_price: @beer.unit_price,
       status: 0,
-      created_at: Date.current,
-      updated_at: Date.current
     )
-
     @transaction_1 = @invoice_1.transactions.create!(
       credit_card_number: "4654405418249632",
       result: "success"
@@ -111,6 +78,10 @@ RSpec.describe "Invoice Show Page" do
       credit_card_number: "4654405418249632",
       result: "failed"
     )
+
+    @bd_1 = @merchant_1.bulk_discounts.create!(percent_discount: 20, quantity_threshold: 50)
+    @bd_2 = @merchant_1.bulk_discounts.create!(percent_discount: 30, quantity_threshold: 60)
+    @bd_3 = @merchant_1.bulk_discounts.create!(percent_discount: 40, quantity_threshold: 70)
 
     visit "/admin/invoices/#{@invoice_1.id}"
   end
@@ -128,6 +99,12 @@ RSpec.describe "Invoice Show Page" do
       expect(page).to have_content("Quantity: 1")
       expect(page).to have_content("Sold for: $320.0 each")
       expect(page).to have_content("Status: packaged")
+    end
+  end
+
+  it "displays the total revenue to be made after bulk discounts", :vcr do
+    within("#total_discount_revenue") do
+      expect(page).to have_content("Total After Bulk Discounts: $4320.0")
     end
   end
 end
